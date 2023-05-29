4aca9f39a63dc0cadefa00fd69e3bca49ef67aeb
MironBerch <mironberchetov@gmail.com>
2023-05-29 19:14:49 +0300

    Update search url

--- social_network/search/urls.py
+++ social_network/search/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from posts.views import (
-
-)
+from search.views import SearchAPIView
 
 
 urlpatterns = [
     path(
         route='', 
-        view=PostAPIView.as_view(), 
-        name='post'),
+        view=SearchAPIView.as_view(), 
+        name='search',
+    ),
 ]
